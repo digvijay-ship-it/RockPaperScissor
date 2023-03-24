f921e38acf5a266710e69398ccc8211dbf7b99aa
Digvijay Singh <dssodha1234@gmail.com>
2023-03-24 21:36:15 +0530

    code refactor and now game return winner insterad of peinting result

--- Script.js
+++ Script.js
@@ -6,37 +6,44 @@ function getComputerChoice(){
     return randomNumberForComputer ===0? "Rock":randomNumberForComputer ===1? "Paper":randomNumberForComputer ===2? "Scissor":false;
 }
 function playground(playerChoice,computerChoice){
-    console.log("player "+playerChoice)
-    console.log("Computer "+computerChoice)
+    
     if(playerChoice==="rock"&&computerChoice!=="rock"){
         if(computerChoice==="scissor"){
-            console.log("Player Wins");
+            return "player";
         }
         else{
-            console.log("Computer Wins");
+            return "computer";
         }
     }
     else if(playerChoice==="paper"&&computerChoice!=="paper"){
         if(computerChoice==="rock"){
-            console.log("Player Wins");
+            return "player";
         }
         else{
-            console.log("Computer Wins");
+            return "computer";
         }
     }
     else if(playerChoice==="scissor"&&computerChoice!=="scissor"){
         if(computerChoice==="paper"){
-            console.log("Player Wins");
+            return "player";
         }
         else{
-            console.log("Computer Wins");
+            return "computer";
         }
     }
     else if(playerChoice===computerChoice){
-        console.log("Its A draw!");
+        return "Its A draw!";
     }
 }
 
-const playerChoice = prompt("Enter one if'Rock','Paper' and 'Scissor'","Rock").toLocaleLowerCase();
-const computerChoice = getComputerChoice().toLocaleLowerCase();
-playground(playerChoice,computerChoice)
+function PlayRockPaperScissor(){
+    const playerChoice = prompt("Enter one if'Rock','Paper' and 'Scissor'","Rock").toLowerCase();
+    const computerChoice = getComputerChoice().toLowerCase();
+    console.log(playground(playerChoice,computerChoice));
+}
+function main(){
+    for(let i=0;i<5;i++){
+        PlayRockPaperScissor();
+    }
+}
+main();
