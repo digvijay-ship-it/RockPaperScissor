7cdf154c8183b4c068244100e84db316e28457ea
Digvijay Singh <dssodha1234@gmail.com>
2023-04-01 12:33:02 +0530

    Refactor script file and make the game run once
    Script file is refactored and plus the game will be running for once
    -Introduced this change for testing

--- Script.js
+++ Script.js
@@ -1,49 +1,49 @@
-function getRandomNumber(){
-    return Math.floor(Math.random()*3);   
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3);
 }
-function getComputerChoice(){
+function getComputerChoice() {
     let randomNumberForComputer = getRandomNumber();
-    return randomNumberForComputer ===0? "Rock":randomNumberForComputer ===1? "Paper":randomNumberForComputer ===2? "Scissor":false;
+    return randomNumberForComputer === 0 ? "Rock" : randomNumberForComputer === 1 ? "Paper" : randomNumberForComputer === 2 ? "Scissor" : false;
 }
-function playground(playerChoice,computerChoice){
-    
-    if(playerChoice==="rock"&&computerChoice!=="rock"){
-        if(computerChoice==="scissor"){
+function playground(playerChoice, computerChoice) {
+
+    if (playerChoice === "rock" && computerChoice !== "rock") {
+        if (computerChoice === "scissor") {
             return "player";
         }
-        else{
+        else {
             return "computer";
         }
     }
-    else if(playerChoice==="paper"&&computerChoice!=="paper"){
-        if(computerChoice==="rock"){
+    else if (playerChoice === "paper" && computerChoice !== "paper") {
+        if (computerChoice === "rock") {
             return "player";
         }
-        else{
+        else {
             return "computer";
         }
     }
-    else if(playerChoice==="scissor"&&computerChoice!=="scissor"){
-        if(computerChoice==="paper"){
+    else if (playerChoice === "scissor" && computerChoice !== "scissor") {
+        if (computerChoice === "paper") {
             return "player";
         }
-        else{
+        else {
             return "computer";
         }
     }
-    else if(playerChoice===computerChoice){
+    else if (playerChoice === computerChoice) {
         return "Its A draw!";
     }
 }
 
-function PlayRockPaperScissor(){
-    const playerChoice = prompt("Enter one if'Rock','Paper' and 'Scissor'","Rock").toLowerCase();
+function PlayRockPaperScissor() {
+    const playerChoice = prompt("Enter one if'Rock','Paper' and 'Scissor'", "Rock").toLowerCase();
     const computerChoice = getComputerChoice().toLowerCase();
-    console.log(playground(playerChoice,computerChoice));
+    console.log(playground(playerChoice, computerChoice));
 }
-function main(){
-    for(let i=0;i<5;i++){
-        PlayRockPaperScissor();
-    }
+function main() {
+    // for(let i=0;i<5;i++){
+    PlayRockPaperScissor();
+    // }
 }
 main();
