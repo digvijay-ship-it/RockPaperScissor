130aaff22be3cc708f60b7bf7c6897730daaf50b
Digvijay Singh <dssodha1234@gmail.com>
2023-04-02 10:10:46 +0530

    Declare winner based on score board and reset game
    Winner of contest is declared based on score board and once is declared
    game resets

--- Script.js
+++ Script.js
@@ -35,20 +35,41 @@ function playground(playerChoice, computerChoice) {
         return "Its A draw!";
     }
 }
+function resetGame(){
+    playerWinCount=0;
+    computerWinCount=0;
+}
+function declareWinner(winner) {
+    //create element
+    const contestWinner = document.createElement("h1");
+    contestWinner.innerText = "Winner of this contest is " + winner;
+    //insert it as #contestWinner div's child
+    document.querySelector("#Winner").appendChild(contestWinner);
+}
 let playerWinCount = 0;
 let computerWinCount = 0;
+
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
     button.addEventListener("click", event => {
         const pC = (event.target.innerText).toLowerCase();
         const winner = PlayRockPaperScissor(pC);
+
         if (winner === "player") {
             playerWinCount++;
-            document.querySelector("#playerScore").innerText = `${playerWinCount}`;
+            document.querySelector("#playerScore").innerText = `Player score: ${playerWinCount}`;
         }
         else if (winner === "computer") {
             computerWinCount++;
-            document.querySelector("#computerScore").innerText = `${computerWinCount}`;
+            document.querySelector("#computerScore").innerText = `Computer score: ${computerWinCount}`;
+        }
+        if (playerWinCount === 5) {
+            declareWinner("player");
+            resetGame();
+        }
+        if (computerWinCount === 5) {
+            declareWinner("computer");
+            resetGame();
         }
     });
 });
